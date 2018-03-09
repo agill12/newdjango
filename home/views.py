5d8b19e469f4d5e2e641709d65ca7758704d1596
Anne Gill <gill01730@gmail.com>
2018-03-09 09:48:10 +0000

    First Commit

---
+++ home/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def get_index(request):
+    return render(request, "home/index.html")
+
+@login_required()    
+def secrets(request):
+    
+    
+    return render(request, "home/secret.html")
