e8513c39cbec2e1a03900d2b2b047c32a8cbacac
Anne Gill <gill01730@gmail.com>
2018-03-12 12:32:52 +0000

    Added Testing

--- messenger/tests.py
+++ messenger/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from messenger.views import inbox,sent,view_message,compose
+from django.core.urlresolvers import resolve
+
 
 # Create your tests here.
+class Messenger_Tests(TestCase):
+    def test_inbox_resolves(self):
+        inbox_res=resolve('/messenger/inbox/')
+        self.assertEqual(inbox_res.func,inbox)
+
+
+    def test_sent_resolves(self):
+        sent_res=resolve('/messenger/sent/')
+        self.assertEqual(sent_res.func,sent)
+
+    def test_view_resolves(self):
+        view_res=resolve('/messenger/view_message/1/')
+        self.assertEqual(view_res.func,view_message)
+
+
+    def test_compose_resolves(self):
+        compose_res=resolve('/messenger/compose/')
+        self.assertEqual(compose_res.func,compose)
+        
+    def test_message_resolves_id(self):
+        error_res=self.client.get('/messenger/view_message')
+        self.assertEqual(error_res.status_code,404)
+        
+  
+        
+        
