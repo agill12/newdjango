0c952a40f5259c17db2db575c47911de1fb93c94
Anne Gill <gill01730@gmail.com>
2018-03-13 10:32:09 +0000

    Test Fail

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class TestAccounts(TestCase):
+    def test_anonymous_user_can_not_view_profile(self):
+        response = self.client.get('/accounts/profile/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_logged_in_user_can_view_profile(self):
+        user = User.objects.create_user('user', 'user@example.com', 'user')
+        self.client.login(username="user", password="user")
+
+        response = self.client.get('/accounts/profile/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "accounts/profile.html")
